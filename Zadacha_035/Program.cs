61a9b3d10a21568fb2d383c5f24efd3d5e99264c
Антон Дик <antondik@yandex.ru>
2022-12-24 15:18:44 +0300

    Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].

---
+++ Zadacha_035/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+Console.Clear();
+const int SIZE = 123;
+int[] array = new int[SIZE];
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // [-9; 10)
+    }
+    return arr;
+}
+
+int FindNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {  
+            count++;
+        }
+    }
+    return count;
+}
+
+array = FillArrayWithRandomNumbers(SIZE, -100, 100);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int count = FindNumber(array);
+System.Console.WriteLine($"В масиве присутсвует от 10 до 99 - {count} чисел");
